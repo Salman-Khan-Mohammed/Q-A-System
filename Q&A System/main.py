e1db2f916df4ca647cf856918a487c13862c3ac0
Salman_mohammed <mdnawaz931@gmail.com>
2024-08-07 23:10:07 -0400

    Frontend part started

--- Q&A System/main.py
+++ Q&A System/main.py
@@ -1,19 +1,23 @@
 import streamlit as st
 from langchain_helper import get_qa_chain, create_vector_db
+from sentence_transformers import SentenceTransformer
+
 
 st.title("Codebasics Q&A 🌱")
 btn = st.button("Create Knowledgebase")
 if btn:
-    create_vector_db()
+    # create_vector_db()
+    pass
 
 question = st.text_input("Question: ")
 
 if question:
-    chain = get_qa_chain()
-    response = chain(question)
+    pass
+#     chain = get_qa_chain()
+#     response = chain(question)
 
-    st.header("Answer")
-    st.write(response["result"])
+#     st.header("Answer")
+#     st.write(response["result"])
 
 
 
